e8accbcd5b27ecf78840e67df414c4e696abdf17
syzsky <ld1689@163.com>
2026-09-20 18:18:30 +0800

    fix(update): 应用内检测更新指向本仓库 Release（syzsky/webhtv）

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,11 +2,11 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    private static final String GITHUB_LATEST = "https://github.com/fish2018/webhtv/releases/latest/download";
-    private static final String GITHUB_RELEASE = "https://github.com/fish2018/webhtv/releases/download";
-    private static final String GITHUB_API = "https://api.github.com/repos/fish2018/webhtv/releases/tags";
-    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/fish2018/webhtv/releases";
-    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/fish2018/webhtv/releases/assets";
+    private static final String GITHUB_LATEST = "https://github.com/syzsky/webhtv/releases/latest/download";
+    private static final String GITHUB_RELEASE = "https://github.com/syzsky/webhtv/releases/download";
+    private static final String GITHUB_API = "https://api.github.com/repos/syzsky/webhtv/releases/tags";
+    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/syzsky/webhtv/releases";
+    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/syzsky/webhtv/releases/assets";
     private static final String CNB = "https://cnb.cool/fish2035/webhtv-release/-/git/raw/main";
 
     public static String getCnbAsset(String name) {
